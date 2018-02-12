07ac0485790f0796a63f1524b5f8e61bc2aa0a36
joelai <joelai@localhost>
2018-02-12 16:16:19 +0800

    rework with react-route and demo sub route and pass props

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import {HashRouter as Router /*, Switch */, Route, Link as Link} from 'react-router-dom';
 import Datepicker from './datepicker.jsx';
 import Photopicker from './photopicker.jsx';
@@ -9,7 +9,7 @@ import NotiTest from './notitest.jsx';
 import * as Ctrl from './ctrl';
 import imgWikiDrawing from './img/wiki_drawing.png';
 
-class Submenu extends React.Component {
+class Misc1 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -17,86 +17,147 @@ class Submenu extends React.Component {
   }
 
   render() {
-    return (<Router>
+    console.log('Misc1.this: ', this);
+    return (<div>
+      <div className='nav'>
+        <div className='item'>
+          <Link to={`${this.props.match.url}/Misc11`}>Misc11</Link>
+        </div>
+        <div className='sep'>|</div>
+        <div className='item'>
+          <Link to={`${this.props.match.url}/Misc12`}>Misc12</Link>
+        </div>
+      </div>
+      Routed here and pass props from: {this.props.from}
       <div>
+        <Route path={`${this.props.match.path}/:misc1Page`} render={(props) => {
+          console.log('Misc1 page: ', props);
+          return (<div style={{
+            backgroundColor: '#0041be',
+            color: '#cfffc9'
+          }}>
+            Request Page: {props.match.params.misc1Page}
+          </div>);
+        }}/>
+      </div>
+    </div>);
+  }
+}
+
+Misc1.propTypes = {
+  match: PropTypes.object,
+  from: PropTypes.string
+};
+
+class Misc extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    console.log('Misc.this: ', this);
+    return (<div>
+      <div className='nav'>
+        <div className='item'>
+          <Link to={`${this.props.match.url}/Misc1`}>Misc1</Link>
+        </div>
+        <div className='sep'>|</div>
+        <div className='item'>
+          <Link to={`${this.props.match.url}/Misc2`}>Misc2</Link>
+        </div>
+      </div>
+      <div>
+        <Route path={`${this.props.match.path}/:miscPage`} render={(props) => {
+          console.log('Misc page: ', props);
+          return (<div style={{
+            backgroundColor: '#0041be',
+            color: '#cfffc9'
+          }}>
+            Request Page: {props.match.params.miscPage}
+          </div>);
+        }}/>
+        <Route path={`${this.props.match.path}/Misc1`} render={(props) => {
+          return (<Misc1 {...props} from={this.props.match.path}/>);
+        }}/>
+      </div>
+    </div>);
+  }
+}
+
+Misc.propTypes = {
+  match: PropTypes.object
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    console.log('App.this: ', this);
+    return (<div>
+      <div className='nav'>
+        <div className='title inline'>
+          <Link to='/'>
+            <img className='thumb' src={imgWikiDrawing}/>
+          </Link>
+          <span>Demo</span>
+        </div>
+        <div className='item'>
+          <Link to='/Photopicker'>Photopicker</Link>
+        </div>
+        <div className='sep'>|</div>
+        <div className='item'>
+          <Link to='/Datepicker'>Datepicker</Link>
+        </div>
+        <div className='sep'>|</div>
+        <div className='item'>
+          <Link to='/Locpicker'>Locpicker</Link>
+        </div>
+        <div className='sep'>|</div>
         <div className='item'>
           <Link to='/NotiTest'>Push notification</Link>
         </div>
+        <div className='sep'>|</div>
+        <div className='item'>
+          <Link to='/Misc'>Miscellaneous</Link>
+        </div>
       </div>
-    </Router>);
-  }
-}
-
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    return (null);
-  }
-}
-
-export class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    return (<Router>
       <div>
-        <div className='nav'>
-          <div className='title inline'>
-            <img className='thumb' src={imgWikiDrawing}/>
-            <span>Demo</span>
-          </div>
-          <div className='item'>
-            <Link to='/'>Home</Link>
-          </div>
-          <div className='sep'>|</div>
-          <div className='item'>
-            <Link to='/Photopicker'>Photopicker</Link>
-          </div>
-          <div className='sep'>|</div>
-          <div className='item'>
-            <Link to='/Datepicker'>Datepicker</Link>
-          </div>
-          <div className='sep'>|</div>
-          <div className='item'>
-            <Link to='/Locpicker'>Locpicker</Link>
-          </div>
-          <div className='sep'>|</div>
-          <div className='item'>
-            <Link to='/NotiTest'>Push notification</Link>
-          </div>
-          <div className='sep'>|</div>
-          <div className='item'>
-            <Link to='/Submenu'>Submenu</Link>
-          </div>
-        </div>
-        <div>
-          <Route path='/' exact component={Home}/>
-          <Route path='/Photopicker' component={Photopicker}/>
-          <Route path='/Datepicker' component={Datepicker}/>
-          <Route path='/Locpicker' component={Locpicker}/>
-          <Route path='/NotiTest' component={NotiTest}/>
-          <Route path='/Submenu' component={Submenu}/>
-        </div>
+        <Route path={`/:demoPage`} render={(props) => {
+          console.log('App page: ', props);
+          return (<div style={{
+            backgroundColor: '#0041be',
+            color: '#cfffc9'
+          }}>
+            Request Page: {props.match.params.demoPage}
+          </div>);
+        }}/>
+        <Route path='/Photopicker' component={Photopicker}/>
+        <Route path='/Datepicker' component={Datepicker}/>
+        <Route path='/Locpicker' component={Locpicker}/>
+        <Route path='/NotiTest' component={NotiTest}/>
+        <Route path='/Misc' component={Misc}/>
       </div>
-    </Router>);
+    </div>);
   }
 }
+
+App.propTypes = {
+  match: PropTypes.object
+};
 
 function launch() {
   var holder = document.createElement('div');
   document.body.appendChild(holder);
-  ReactDOM.render((<App/>), holder);
+  ReactDOM.render((<Router>
+    <App/>
+  </Router>), holder);
 }
 
 // console.log('window.isCordovaApp: ', window.isCordovaApp);
